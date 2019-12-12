60f044144fe15c271fca299d25826370b4ec800f
Administrator <Administrator@DESKTOP-223RLLA>
2019-12-12 15:36:23 +0800

    啦啦

--- Snoopy.WebForm/User/Login.aspx.cs
+++ Snoopy.WebForm/User/Login.aspx.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
+﻿
+using System;
 namespace Snoopy.WebForm.User
 {
     public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            string code = Request.QueryString["code"] + "";
+            if (code!="")
+            {
+                //tyLogin.CallLogin("GitHub", code);
+            }
+            
         }
     }
 }
